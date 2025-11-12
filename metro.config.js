6c170fd1ce7d04db0d268b7fe2373ae2fb404e35
silkpanda <anthony.ha90@gmail.com>
2025-11-11 20:19:19 -0600

    stable tailwind build

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-// This wraps the default config and adds the NativeWind compiler.
-// We must specify the 'input' as our global.css file[cite: 71, 93].
-module.exports = withNativeWind(config, { input: "./global.css" });
