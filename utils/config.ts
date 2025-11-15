3c701353bf572eb184edc0ae13a851697ec53de8
silkpanda <anthony.ha90@gmail.com>
2025-11-15 02:09:48 -0600

    reset

--- utils/config.ts
+++
@@ -1,7 +0,0 @@
-// --- THIS IS THE CRITICAL V4 CHANGE (Step 3.2) ---
-// The mobile app MUST talk to the BFF, not the API directly.
-// We are changing the port from 3001 (API) to 3002 (BFF).
-export const API_BASE_URL = 'http://localhost:3002';
-// --- END OF CHANGE ---
-
-// You can export other global config variables here
